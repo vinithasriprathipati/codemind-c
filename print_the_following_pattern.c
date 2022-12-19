432ca5dd777783848b23bc1cd4dac7c24eba5b6e
Vinitha Sri Prathipati <119840677+vinithasriprathipati@users.noreply.github.com>
2022-12-19 20:59:08 +0530

    #include<stdio.h> int main() {     int n;     char i,j;     scanf("%d",&n);     for(i=64+n;i>=65;i--)     {         for(j=i;j>=65;j--)         printf("%c ",i);         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
-int main() 
+#include<stdio.h>
+int main()
 {
-    int n,i,j;
+    int n;
+    char i,j;
     scanf("%d",&n);
-    for(i=65;i<65+n;i++)
+    for(i=64+n;i>=65;i--)
     {
-        for(j=1;j<=n;j++)
-        {
-            printf("%c ",i);
-        }
+        for(j=i;j>=65;j--)
+        printf("%c ",i);
         printf("
 ");
     }
