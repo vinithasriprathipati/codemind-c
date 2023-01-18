845eed832ee5ceb28cf763573c00f2e246c4daa6
Vinitha Sri Prathipati <119840677+vinithasriprathipati@users.noreply.github.com>
2023-01-18 18:00:06 +0530

    #include<stdio.h> int main() {     int n,i,z,j,c,k;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=i;j++)         {             if(j==1 || j==i || i==n)             printf("*");             else             printf(" ");
            }
            printf("
    ");
        }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int n,i,z,j,c,k;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-           for(j=1;j<=n;j++)
-           {
-              if(j==i || j==n+1-i)
-              printf("x");
-              else
-              printf("0");
-            }
-            printf("
+        for(j=1;j<=i;j++)
+        {
+            if(j==1 || j==i || i==n)
+            printf("*");
+            else
+            printf(" ");
+        
+        }
+        printf("
 ");
     }
 }
